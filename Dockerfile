bb24f088642440f1baa4c37709810ec33aa44f40
Peterlight Faboyede <petlight49@gmail.com>
2022-01-07 18:40:32 +0100

    done

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.9
-WORKDIR /django
-RUN pip3 install --upgrade pip
-COPY requirements.txt requirements.txt
-RUN pip3 install -r requirements.txt
-COPY . .
--- chat_server/asgi.py
+++ chat_server/asgi.py
@@ -1,12 +1,10 @@
 import os
 
-from channels.auth import AuthMiddlewareStack
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_server.settings')
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 from chat.middlewares import TokenAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_server.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
