175c830790d55097d7d6138b567e7717cf7ca61a
Peterlight Faboyede <petlight49@gmail.com>
2022-01-08 12:48:08 +0100

    done

--- chat_server/__init__.py
+++ chat_server/__init__.py
@@ -1,4 +1,6 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv(dotenv_path=os.path.join(os.getcwd(), "env", ".env"))
+print(os.getcwd())
+raise ZeroDivisionError
+load_dotenv("env/.env")
