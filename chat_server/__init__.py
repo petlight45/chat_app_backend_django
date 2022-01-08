2b87cfc31e3815188262065e6a29f4cac2b2964a
Peterlight Faboyede <petlight49@gmail.com>
2022-01-08 13:23:46 +0100

    done

--- chat_server/__init__.py
+++ chat_server/__init__.py
@@ -1,4 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv()
+
+print(os.getcwd())
+raise ZeroDivisionError
+load_dotenv(os.path.join(os.getcwd(), "env", ".env"))
--- chat_server/asgi.py
+++ chat_server/asgi.py
@@ -1,11 +1,18 @@
 import os
+import os
+from pathlib import Path
+from dotenv import load_dotenv
 
+print(os.getcwd())
+raise ZeroDivisionError
+load_dotenv(os.path.join(os.getcwd(), "env", ".env"))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_server.settings')
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import django
+
 django.setup()
 import chat.routing
 from chat.middlewares import TokenAuthMiddleware
